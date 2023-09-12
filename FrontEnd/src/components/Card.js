0df2c77fbec62207e6dd545331eaf9466f0ebac0
Raj <87857226+Axxi3@users.noreply.github.com>
2023-09-12 20:27:44 +0530

    Revert "#1  Nav is fully responsive now / no major refacotre"

--- FrontEnd/src/components/Card.js
+++ FrontEnd/src/components/Card.js
@@ -1,19 +1,17 @@
-import React from "react";
+ import React from 'react'
 
 export default function Card(props) {
   return (
-    <>
-      <div class="card carder">
-        <img src={props.link} className="card-img-top" alt="..." />
-        <div class="card-body c">
-          <h5 class="card-title">{props.name}</h5>
+   <> 
+   <div class="card carder" >
+  <img src={props.link} class="card-img-top" alt="..." />
+  <div class="card-body c">
+    <h5 class="card-title">{props.name}</h5>   
 
-          <p class="card-text">Written by {props.author}</p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    </>
-  );
+    <p class="card-text">Written by {props.author}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+   </>
+  )
 }
