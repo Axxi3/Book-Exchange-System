460575a15eb1385ad823d6151fccf48fdb12b1f2
Raj <87857226+Axxi3@users.noreply.github.com>
2023-11-13 17:06:58 +0530

    Update CreateUser.js

--- FrontEnd/src/Screen/CreateUser.js
+++ FrontEnd/src/Screen/CreateUser.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { NavLink, useNavigate } from "react-router-dom";  
+import {  useNavigate } from "react-router-dom";  
 import DOMPurify from 'dompurify';
 export default function CreateUser() {  
     const navigate = useNavigate();
