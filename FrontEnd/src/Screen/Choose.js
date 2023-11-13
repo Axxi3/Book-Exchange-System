93661566b28019444956b620e82033ae8f9078b4
Raj <87857226+Axxi3@users.noreply.github.com>
2023-11-13 17:06:47 +0530

    Update Choose.js

--- FrontEnd/src/Screen/Choose.js
+++ FrontEnd/src/Screen/Choose.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 export default function Choose() {  
 const navigate=useNavigate()
     const addproduct=()=>{ 
