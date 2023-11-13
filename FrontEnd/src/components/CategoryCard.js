39cbe660dcc1b0255beefc227943474ce457e023
Raj <87857226+Axxi3@users.noreply.github.com>
2023-11-13 17:02:58 +0530

    Update CategoryCard.js

--- FrontEnd/src/components/CategoryCard.js
+++ FrontEnd/src/components/CategoryCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function CategoryCard(props) {    
-  const { CategoryName, click } = props;     
+  // const {  click } = props;     
   return (
     <li onClick={() => click(props.name)}>
       <p>{props.name}</p>
