b20fe51c1237cfbcfe8fc9f107d346f94f4378e8
Raj <87857226+Axxi3@users.noreply.github.com>
2023-11-13 17:06:29 +0530

    Update Addproduct.js

--- FrontEnd/src/Screen/Addproduct.js
+++ FrontEnd/src/Screen/Addproduct.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react'
+import React, { useEffect, useState} from 'react'
 import {  decodeToken } from "react-jwt";
 export default function Addproduct() {      
   const[auth,setauth]=useState("") 
